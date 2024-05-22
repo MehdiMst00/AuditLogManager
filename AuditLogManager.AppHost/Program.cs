827d69993e2972f1a514065ccf5e9dec4d3f5bd9
Mohammad Mehdi Mostatabi <mohammadmehdi.mst@outlook.com>
2024-05-23 01:16:10 +0330

    Add sample project

--- AuditLogManager.AppHost/Program.cs
+++ AuditLogManager.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.AuditLogManager>("auditlogmanager");
 
+builder.AddProject<Projects.AuditLogManager_Sample_AspNetCore>("auditlogmanager-sample-aspnetcore");
+
 builder.Build().Run();
